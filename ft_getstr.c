8cd2637c1b1ffae58734a32d344f43aec5e286f9
Dylan SLOGROVE <dslogrov@e4r3p4.wethinkcode.co.za>
2018-06-11 15:51:58 +0200

    stuff

--- ft_getstr.c
+++ ft_getstr.c
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/05 16:30:21 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/06/11 13:29:41 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/06/11 15:43:59 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,17 @@
 static char	*ft_getstr_str(t_printf_args args)
 {
 	const char		*str = va_arg(*args.args, char *);
-	const size_t	len = ft_strlen(str);
-	char			*temp;
-	int				chars;
 
 	args.flags &= ~SIGN_PLUS;
 	args.flags &= ~SIGN_SPACE;
 	args.flags &= ~FORCE_STYLE;
 	args.flags &= ~PAD_ZERO;
-	if (!args.precision)
-		args.precision = len;
-	if (args.format = 'S' || args.flags << 5 == LONG_INT)
-	{
-		temp = str;
-		chars = 0;
-		while (*temp && chars <= args.precision)
-			if (*(temp++) > 127)
-				args.precision++;
-			else
-				chars++;
-	}
 	return (ft_strdup(str));
 }
 
 static char	*ft_getstr_dec(t_printf_args args)
 {
 	const unsigned char len = args.flags << 5;
-	// TODO: get atoi to work with intmax+
 	args.flags &= ~FORCE_STYLE;
 	if (len == LONG_INT || args.format == 'D')
 		return (ft_itoa(va_arg(*args.args, long int)));
@@ -65,8 +49,10 @@ static char	*ft_getstr_u(t_printf_args args, unsigned char base)
 {
 	const unsigned char len = args.flags << 5;
 
-	if (args.format = 'p')
+	if (args.format == 'p')
 		args.flags &= FORCE_STYLE;
+	if (ft_tolower(args.format) == 'u')
+		args.flags |= ~FORCE_STYLE;
 	if (len == LONG_INT || args.format == 'O' || args.format == 'U')
 		return (ft_itoa_base(va_arg(*args.args, unsigned long int), base));
 	if (len == INT)
@@ -94,7 +80,7 @@ static char	*ft_getstr_char(t_printf_args args)
 	str = ret;
 	if (i < 0)
 		return (str);
-	else if (i <= 0x7F || args.format == 'c')
+	else if (i <= 0x7F || (args.format == 'c' && args.flags << 5 != LONG_INT))
 		*str = (unsigned char)i;
 	else if (i <= 0x7FF)
 	{
@@ -115,7 +101,9 @@ static char	*ft_getstr_char(t_printf_args args)
 char		*ft_getstr_all(t_printf_args args)
 {
 	const char f = args.format;
-
+	
+	if (args.precision)
+		args.flags |= ~PAD_ZERO;
 	if (f == 's' || f == 'S')
 		return (ft_getstr_str(args));
 	if (f == 'c' || f == 'C')
@@ -124,9 +112,10 @@ char		*ft_getstr_all(t_printf_args args)
 		return (ft_getstr_dec(args));
 	if (f == 'u' || f == 'U')
 		return (ft_getstr_u(args, 10));
-	//TODO: get atoi_base to work with 'x' specifier
-	if (f == 'p' || f == 'x' || f == 'X')
+	if (f == 'X')
 		return (ft_getstr_u(args, 16));
+	if (f == 'p' || f == 'x')
+		return (ft_getstr_u(args, -16));
 	if (f == 'o' || f == 'O')
 		return (ft_getstr_u(args, 8));
 }
--- ft_putarg.c
+++ ft_putarg.c
@@ -6,39 +6,67 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/10 15:41:37 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/06/11 13:26:21 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/06/11 15:50:22 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putarg(t_printf_args a)
+static void	ft_putarg_str(t_printf_args a, const char *str)
+{
+	const size_t	len = ft_strlen(str);
+
+	if (!a.precision || ft_tolower(a.format) == 'c')
+		a.precision = len;
+	if (!(a.flags & LEFT_JUSTIFY))
+		while (a.width-- > min(len, a.precision))
+			ft_putchar(' ');
+	ft_putstr(str);
+	if (a.flags & LEFT_JUSTIFY)
+		while (a.width-- > min(len, a.precision))
+			ft_putchar(' ');
+}
+
+static void	ft_putarg_u(t_printf_args a, const char *str)
+{
+	const size_t	len = ft_strlen(str);
+	
+	if (a.flags & FORCE_STYLE && ft_tolower(a.format) != 'u')
+	{
+		a.width--;
+		if (a.format == 'p' || ft_tolower(a.format == 'x'))
+			a.width--;
+	}
+	if (!(a.flags & LEFT_JUSTIFY) & !(a.flags & PAD_ZERO))
+		while (a.width-- > max(len, a.precision))
+			ft_putchar(' ');
+	if (a.flags & FORCE_STYLE && ft_tolower(a.format) != 'u')
+	{
+		ft_putchar('0');
+		if (a.format == 'X')
+			ft_putchar('X');
+		else if (a.format == 'p' || a.format == 'x')
+			ft_putchar('x');
+	}
+	while (!(a.flags & LEFT_JUSTIFY) && a.flags & PAD_ZERO && a.width-- > len)
+		ft_putchar('0');
+	ft_putstr(str);
+	if (a.flags & LEFT_JUSTIFY)
+		while (a.width-- > max(len, a.precision))
+			ft_putchar(' ');
+}
+
+void			ft_putarg(t_printf_args a)
 {
 	const char		*str = ft_getstr_all(a);
 	const size_t	len = ft_strlen(str);
 
 	a.width--;
-	if (a.flags & FORCE_STYLE && (a.format == 'p' || a.format == 'x'
-		|| a.format == 'X'))
-	{
-		a.width -= write(1, "0x", 2);
-		a.flags &= PAD_ZERO;
-	}
-	else if (a.flags & FORCE_STYLE && (a.format == 'o' || a.format == 'O'))
-	{
-		a.width -= write(1, "0", 1);
-		a.flags &= PAD_ZERO;
-	}
-	while (!(a.flags & LEFT_JUSTIFY) && (a.width-- > len || a.precision-- > len)
-		&& a.flags & PAD_ZERO)
-		ft_putchar('0');
-	while (!(a.flags & LEFT_JUSTIFY) &&
-		(a.width-- > len || a.precision-- > len))
-		ft_putchar(' ');
-//TODO: manage symbols
-//TODO: manage string precision
-	ft_putstr(str);
+	if (ft_tolower(a.format) == 's' || ft_tolower(a.format) == 'c')
+		ft_putarg_str(a, str);
+	else if (ft_tolower(a.format) == 'u' || ft_tolower(a.format) == 'o' ||
+		ft_tolower(a.format) == 'x' || a.format == 'p')
+		ft_putarg_u(a, str);
+	//TODO: manage symbols
 	free(str);
-	while (a.flags & LEFT_JUSTIFY && a.width-- > len)
-		ft_putchar(' ');
 }
