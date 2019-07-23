9cf6635c457d67e8b84100e189fd64bd46ab7c38
Dylan Slogrove <dslogrove@gmail.com>
2019-07-23 18:56:46 +0200

    Fixed hexdump spacing

--- ft_hexdump.c
+++ ft_hexdump.c
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 14:02:23 by dslogrov          #+#    #+#             */
-/*   Updated: 2019/07/17 14:17:08 by dslogrov         ###   ########.fr       */
+/*   Updated: 2019/07/23 18:13:40 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ void	ft_hexdump(void *region, size_t size, off_t offset, int fd)
 	while (i < size)
 	{
 		ft_printf_fd(fd,
-			"%016zx       %02hhx", offset + i, *(char *)(region + i));
+			"%016zx\t%02hhx ", offset + i, *(char *)(region + i));
 		i++;
 		while (i < size && i % 16)
-			ft_printf_fd(fd, " %02hhx", *(char *)(region + i++));
+			ft_printf_fd(fd, "%02hhx ", *(char *)(region + i++));
 		ft_putendl("");
 	}
 }
